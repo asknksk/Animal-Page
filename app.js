272dd18050be29508281622cfd2ee18650d2d956
asknksk <asknkskk@gmail.com>
2022-06-24 13:21:33 +0300

    completed js

---
+++ app.js
@@ -0,0 +1,21 @@
+const imagesDiv = document.querySelector(".container");
+
+function playSound(soundobj) {
+  var thissound = document.querySelector(`.${soundobj}Sound`);
+
+  thissound.play();
+}
+
+function stopSound(soundobj) {
+  var thissound = document.querySelector(`.${soundobj}Sound`);
+  thissound.pause();
+  thissound.currentTime = 0;
+}
+imagesDiv.addEventListener("mouseover", (event) => {
+  const sound = event.target.classList[0];
+  playSound(sound);
+});
+imagesDiv.addEventListener("mouseout", (event) => {
+  const sound = event.target.classList[0];
+  stopSound(sound);
+});
